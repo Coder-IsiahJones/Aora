e294736ee89e0af2381caf01a79545d1d9a00c4d
isiah <jones.isiah.blhs@gmail.com>
2024-05-15 23:24:50 -0400

    add assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Got to profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
